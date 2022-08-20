34bc860e85883f72e2c846dc7cc06d58bbabd703
alexdron101 <info@joie.com.ua>
2022-08-20 15:45:42 +0300

    Update joie-home.vue

--- src/views/joie-home.vue
+++ src/views/joie-home.vue
@@ -7,6 +7,9 @@ import { watch } from 'vue'
 
 
 const works = ref(0)
+function even(works) {
+  return works.filter((item) => item.weight % 2 === 0)
+}
 const pages = ref(0)
 const route = useRoute()
 
@@ -40,16 +43,11 @@ onMounted(() => {
         formatedData['id' + it.id] = it;
       });
       pages.value = formatedData;
-
-
     });
 
-
   startBrain();
-
-
-
 })
+
 
 
 </script>
@@ -73,41 +71,40 @@ onMounted(() => {
 
       <a href="#work" class="scrollto first-a-scroll"><b></b></a>
     </div>
+</div>
 
 
+  <section id="work" class="active">
+    <div id="first-block" class="lust-works">
+      <div id="portfoliolist" class="portfolio-area">
 
-    <section id="work" class="active">
-      <div id="first-block" class="lust-works">
-        <div id="portfoliolist" class="portfolio-area">
 
-
-          <template v-for="item in works">
-            <template v-if="item.is_lust_works === 1 && item.is_published === 1" >
-              <div class="portfolio">
-                <img :src="'https://new.joie.com.ua/storage/' + item.image">
-                <span>
-                  <a target="_blank" href="{{ item.link }}" rel="noopener noreferrer">{{ item.link }}</a>
-                  {{item.weight}}
-                  <p v-if="lang === 'ua'">{{ item.title_ua }}</p>
-                  <p v-if="lang === 'ru'">{{ item.title_ru }}</p>
-                  <p v-if="lang === 'en'">{{ item.title_en }}</p>
-                </span>
-              </div>
-            </template>
+        <template v-for="item in works" :key="item.weight">
+          <template v-if="item.is_lust_works === 1 && item.is_published === 1">
+            <div class="portfolio">
+              <img :src="'https://new.joie.com.ua/storage/' + item.image">
+              <span>
+                <a target="_blank" href="{{ item.link }}" rel="noopener noreferrer">{{ item.link }}</a>
+                <p>Weight = {{ item.weight }}</p>
+                <p>ID = {{ item.id }}</p>
+                <p v-if="lang === 'ua'">{{ item.title_ua }}</p>
+                <p v-if="lang === 'ru'">{{ item.title_ru }}</p>
+                <p v-if="lang === 'en'">{{ item.title_en }}</p>
+              </span>
+            </div>
           </template>
+        </template>
 
 
 
-          <div class="portfolio land wow fadeIn" data-wow-delay="1.2s" data-cat="land">
-            <b class="banak" v-html="pages.id10 && pages.id10['text_' + lang]"></b>
-          </div>
-
+        <div class="portfolio">
+          <b class="banak" v-html="pages.id10 && pages.id10['text_' + lang]"></b>
         </div>
+
       </div>
-    </section>
-  </div>
+    </div>
+  </section>
+  
 </template>
-
-
 
 
